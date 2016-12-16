90e37264c745bae93235b053554f0dcf87403f3a
Adam Zinder <adam.zinder@gmail.com>
2016-12-16 10:49:47 -0800

    add stores and brands on the frontend

---
+++ app.rb
@@ -0,0 +1,42 @@
+require("bundler/setup")
+require('pry')
+Bundler.require(:default)
+Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get('/') do
+  @stores = Store.all
+  @brands = Brand.all
+  erb(:index)
+end
+
+get('/store/new') do
+
+  @brands = Brand.all
+  erb(:store_form)
+end
+
+get('/brand/new') do
+  @stores = Store.all
+  erb(:brand_form)
+end
+
+post('/brands') do
+  name = params['name']
+  type = params['type']
+  @brand = Brand.create({:name => name, :shoe_type => type})
+  @stores = Store.all
+  @brands = Brand.all
+  erb(:index)
+end
+
+post('/stores') do
+  name = params['name']
+  address = params['address']
+  city = params['city']
+  state = params['state']
+  zip_code = params['zip_code']
+  @store = Store.create({:name => name, :street_address => address, :city => city, :state => state, :zip_code => zip_code})
+  @stores = Store.all
+  @brands = Brand.all
+  erb(:index)
+end
--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,1 +1,4 @@
-require 'spec_helper'
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
